af27b2e48a9912c2952cefc0462a907010e8fdb4
Andrey Nikolaenkov <nichexplore@gmail.com>
2026-05-27 13:08:09 +0300

    CRUD read

--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,6 @@ Route::get('about', [PageController::class, 'about'])->name('about');
 // Название сущности в URL во множественном числе, контроллер в единственном
 Route::get('articles', [ArticleController::class, 'index'])
 ->name('articles.index'); // имя маршрута, нужно для того, чтобы не создавать ссылки руками
+
+Route::get('articles/{id}', [ArticleController::class, 'show'])
+->name('articles.show');
